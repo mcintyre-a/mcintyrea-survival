63ff3dc71f8a896b7088d770b620ae0b9e4b489b
mcintyre-a <65350469+mcintyre-a@users.noreply.github.com>
2020-05-18 18:38:41 +0100

    Added in-game progression + bug fixes

--- main.ts
+++ main.ts
@@ -1,26 +1,34 @@
+function gameProgressions () {
+    if (inProgress == 1) {
+        pause(30000)
+        Player1.y += -35
+    }
+}
 function Overtime () {
 	
 }
 function character_Player1 () {
     Player1 = sprites.create(img`
-. . . . . . f f f f . . . . . . 
-. . . . f f f 2 2 f f f . . . . 
-. . . f f f 2 2 2 2 f f f . . . 
-. . f f f e e e e e e f f f . . 
-. . f f e 2 2 2 2 2 2 e e f . . 
-. . f e 2 f f f f f f 2 e f . . 
-. . f f f f e e e e f f f f . . 
-. f f e f b f 4 4 f b f e f f . 
-. f e e 4 1 f d d f 1 4 e e f . 
-. . f e e d d d d d d e e f . . 
-. . . f e e 4 4 4 4 e e f . . . 
-. . e 4 f 2 2 2 2 2 2 f 4 e . . 
-. . 4 d f 2 2 2 2 2 2 f d 4 . . 
-. . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
-. . . . . f f f f f f . . . . . 
-. . . . . f f . . f f . . . . . 
-`, SpriteKind.Player)
-    controller.moveSprite(Player1, 100, 100)
+        . . . . . . f f f f . . . . . .
+        . . . . f f f 2 2 f f f . . . .
+        . . . f f f 2 2 2 2 f f f . . .
+        . . f f f e e e e e e f f f . .
+        . . f f e 2 2 2 2 2 2 e e f . .
+        . . f e 2 f f f f f f 2 e f . .
+        . . f f f f e e e e f f f f . .
+        . f f e f b f 4 4 f b f e f f .
+        . f e e 4 1 f d d f 1 4 e e f .
+        . . f e e d d d d d d e e f . .
+        . . . f e e 4 4 4 4 e e f . . .
+        . . e 4 f 2 2 2 2 2 2 f 4 e . .
+        . . 4 d f 2 2 2 2 2 2 f d 4 . .
+        . . 4 4 f 4 4 5 5 4 4 f 4 4 . .
+        . . . . . f f f f f f . . . . .
+        . . . . . f f . . f f . . . . .
+    `, SpriteKind.Player)
+    Player1.setPosition(6, 103)
+    controller.moveSprite(Player1, 100, 0)
+    Player1.setFlag(SpriteFlag.StayInScreen, true)
 }
 function WELCOME () {
     scene.setBackgroundColor(8)
@@ -39,11 +47,13 @@ function _INIT () {
     info.setScore(5)
     info.setLife(3)
     BoostEval = 0
+    inProgress = 0
 }
 let BoostEval = 0
 let Player1: Sprite = null
+let inProgress = 0
 _INIT()
 WELCOME()
-pause(3000)
 character_Player1()
 info.startCountdown(60)
+inProgress = 1
