f6808e7200fcb2bdf119d39f06b16fbd1ad5f5e1
mcintyre-a <65350469+mcintyre-a@users.noreply.github.com>
2020-05-18 19:54:07 +0100

    Added projectile code

--- main.ts
+++ main.ts
@@ -12,9 +12,6 @@ c 1 b 4 4 4 d c
 . . c c c c . . 
 `, Math.randomRange(0, 100), Math.randomRange(0, 100))
 }
-function makeProjectile () {
-	
-}
 function Overtime () {
     pause(59500)
     if (1 <= info.life()) {
@@ -82,6 +79,28 @@ function WELCOME () {
     game.splash("READY?")
     music.playMelody("F - F - F - C5 C5 ", 120)
 }
+function makeProjectile () {
+    while (inProgress == 1) {
+        Projectile = sprites.createProjectileFromSide(img`
+. . . . . . . . . . . f f f f f f f . . . c c f f f . . . . . . . . . . 
+. . . . . . . . . . f b b b b b b b f f c b b b b f . . . . . . . . . . 
+. . . . . . . . . . f b b 1 1 1 b b b b b f f b f . . . . . . . . . . . 
+. . . . . . . . . . f b 1 1 1 1 1 f f b b b b f f . . . . . . . . . . . 
+. . . . . . . . . . f 1 c c c c 1 f f b b b b b c f f . . . . . . . . . 
+. . . . . . . . . . f f c 1 c 1 c 1 b b c b c b c c c f . . . . . . . . 
+. . . . . . . . . . . f c c 3 3 3 1 b b b c b c b c c c f . . c c c c c 
+. . . . . . . . . . . . c 3 3 3 c 1 b b b c b c b c c c c f c d d b b c 
+. . . . . . . . . . . . c 3 3 3 c 1 b b b b b b b c c c c c d d b c c . 
+. . . . . . . . . . . . c 3 3 3 c 1 1 b b b b b c c c c c c b b c c . . 
+. . . . . . . . . . . c c 3 3 1 c 1 1 b b b b c c c c c c f b c c f . . 
+. . . . . . . . . . . c c 1 3 c 1 1 c b b b c c c c c b b c f c c f . . 
+. . . . . . . . . . . c 1 1 1 1 1 1 c b b b f d d d d d c . f b b c f . 
+. . . . . . . . . . . . c c 1 1 1 1 f b d b b f d d d c . . . f b b f . 
+. . . . . . . . . . . . . . c c c f f f b d b b f c c . . . . . f b b f 
+. . . . . . . . . . . . . . . . . . . . f f f f f . . . . . . . . f f f 
+`, Math.randomRange(0, 100), Math.randomRange(0, 100))
+    }
+}
 function overtimeBonus_points () {
     inProgress = 0
     Player1.setVelocity(70, 70)
@@ -129,12 +148,17 @@ function Boost () {
     invincibleEval = 1
     effects.starField.startScreenEffect()
     Player1.startEffect(effects.rings)
+    music.playMelody("C5 G C5 F C5 G C5 F ", 300)
     scene.cameraShake(4, 500)
     pause(5000)
+    music.stopAllSounds()
+    effects.starField.endScreenEffect()
+    effects.clearParticles(Player1)
     BoostEval = 0
     invincibleEval = 0
 }
 let OT_projectile: Sprite = null
+let Projectile: Sprite = null
 let invincibleEval = 0
 let Player1: Sprite = null
 let foodProjectile: Sprite = null
@@ -152,5 +176,6 @@ while (BoostEval == 0 && info.life() > 0) {
     pause(5000)
 }
 while (true) {
-	
+    makeProjectile()
+    pause(500)
 }
